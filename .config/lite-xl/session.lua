432cad37ff3a1038c8329f95084ed1ae510007bd
talal12bfk <talal12bfk@gmail.com>
2023-09-17 06:52:42 +0300

    Added i3 and lite-xl config files

---
+++ .config/lite-xl/session.lua
@@ -0,0 +1,1 @@
+return {recents={}, window={[1]=1412,[2]=361,[3]=2,[4]=22,["n"]=4}, window_mode="normal", previous_find={[1]=" "}, previous_replace={[1]="/n"}}
---
+++ .config/lite-xl/user_settings.lua
@@ -0,0 +1,85 @@
+return {
+  ["config"] = {
+    ["animation_rate"] = 1.0,
+    ["borderless"] = false,
+    ["code_font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "Hack Nerd Font Mono Regular",
+          ["path"] = "/data/data/com.termux/files/home/.config/lite-xl/fonts/HackNerdFontMono-Regular.ttf"
+        },
+        [2] = {
+          ["name"] = "JetBrains Mono Regular",
+          ["path"] = "/data/data/com.termux/files/usr/share/lite-xl/fonts/JetBrainsMono-Regular.ttf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "subpixel",
+        ["bold"] = false,
+        ["hinting"] = "slight",
+        ["italic"] = false,
+        ["size"] = 15,
+        ["smoothing"] = false,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["disabled_plugins"] = {},
+    ["enabled_plugins"] = {
+      ["autocomplete"] = true,
+      ["ghmarkdown"] = true
+    },
+    ["font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "Hack Nerd Font Regular",
+          ["path"] = "/data/data/com.termux/files/home/.config/lite-xl/fonts/HackNerdFont-Regular.ttf"
+        },
+        [2] = {
+          ["name"] = "Fira Sans Regular",
+          ["path"] = "/data/data/com.termux/files/usr/share/lite-xl/fonts/FiraSans-Regular.ttf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "subpixel",
+        ["bold"] = false,
+        ["hinting"] = "slight",
+        ["italic"] = false,
+        ["size"] = 15,
+        ["smoothing"] = false,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["plugins"] = {
+      ["lineguide"] = {
+        ["custom_color"] = {
+          [1] = 72,
+          [2] = 72,
+          [3] = 79,
+          [4] = 255
+        }
+      },
+      ["minimap"] = {
+        ["caret_color"] = {
+          [1] = 147,
+          [2] = 221,
+          [3] = 250,
+          [4] = 255
+        },
+        ["draw_background"] = false,
+        ["instant_scroll"] = true,
+        ["selection_color"] = {
+          [1] = 82,
+          [2] = 82,
+          [3] = 87,
+          [4] = 255
+        },
+        ["syntax_highlight"] = false,
+        ["width"] = 90
+      }
+    },
+    ["theme"] = "dracula",
+    ["transitions"] = false
+  }
+}
